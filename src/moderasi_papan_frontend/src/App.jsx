2426e591244662555569c6a60c9eb0637651a0c6
Muhammad Saidi <muhammad@example.com>
2025-07-06 17:10:01 +0800

    add login

--- src/moderasi_papan_frontend/src/App.jsx
+++ src/moderasi_papan_frontend/src/App.jsx
@@ -1,31 +1,112 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { AuthClient } from '@dfinity/auth-client';
 import { moderasi_papan_backend } from 'declarations/moderasi_papan_backend';
 
-function App() {
-  const [greeting, setGreeting] = useState('');
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    moderasi_papan_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
+
+const identityProvider =
+
+  process.env.DFX_NETWORK === 'ic'
+
+    ? 'https://identity.ic0.app'
+    : 'http://uxrrr-q7777-77774-qaaaq-cai.localhost:4943/';
+
+
+
+function App() {
+
+  const [authClient, setAuthClient] = useState(null);
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const [principal, setPrincipal] = useState('');
+
+
+
+  useEffect(() => {
+
+    AuthClient.create().then((client) => {
+
+      setAuthClient(client);
+
+      client.isAuthenticated().then(setIsAuthenticated);
+
     });
-    return false;
-  }
+
+  }, []);
+
+
+
+  const login = async () => {
+
+    await authClient.login({
+
+      identityProvider,
+
+      onSuccess: async () => {
+
+        setIsAuthenticated(true);
+
+        const identity = authClient.getIdentity();
+
+        const principalId = identity.getPrincipal().toText();
+
+        setPrincipal(principalId);
+
+      },
+
+    });
+
+  };
+
+
+  const logout = async () => {
+
+    await authClient.logout();
+
+    setIsAuthenticated(false);
+
+    setPrincipal('');
+
+  };
+
+
 
   return (
+
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+
+      <h1>Login Internet Identity</h1>
+
+      {!isAuthenticated ? (
+
+        <button onClick={login}>Login with Internet Ident
+
+          ity</button>
+
+      ) : (
+
+        <>
+
+          <div>
+
+            <strong>Principal:</strong> {principal}
+
+          </div>
+
+          <button onClick={logout}>Logout</button>
+
+        </>
+
+      )}
+
     </main>
+
   );
+
 }
+
+
 
 export default App;
